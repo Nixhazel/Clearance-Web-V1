1708fe4065b767f92715015525e6e12e473fa295
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2021-09-08 02:27:20 +0100

    Initial Website Without backend

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var sentrequest = document.querySelectorAll(".startclearancep");
+
+
+sentrequest[0].addEventListener("click", function(){
+  sentrequest[0].innerHTML ="SENT";
+} );
+sentrequest[1].addEventListener("click", function(){
+  sentrequest[1].innerHTML ="SENT";
+} );
+sentrequest[2].addEventListener("click", function(){
+  sentrequest[2].innerHTML ="SENT";
+} );
+sentrequest[3].addEventListener("click", function(){
+  sentrequest[3].innerHTML ="SENT";
+} );
+sentrequest[4].addEventListener("click", function(){
+  sentrequest[4].innerHTML ="SENT";
+} );
+sentrequest[5].addEventListener("click", function(){
+  sentrequest[5].innerHTML ="SENT";
+} );
+sentrequest[6].addEventListener("click", function(){
+  sentrequest[6].innerHTML ="SENT";
+} );
+sentrequest[7].addEventListener("click", function(){
+  sentrequest[7].innerHTML ="SENT";
+} );
+sentrequest[8].addEventListener("click", function(){
+  sentrequest[8].innerHTML ="SENT";
+} );
